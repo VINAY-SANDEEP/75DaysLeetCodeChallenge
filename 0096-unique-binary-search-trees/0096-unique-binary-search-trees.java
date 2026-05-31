f449b32e15a9d36c39fd4eaa1ec8e5d5e50fb630
Vinay-Sandeep <133085003+VINAY-SANDEEP@users.noreply.github.com>
2026-05-31 18:03:09 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.41%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] uniqTree = new int[n + 1];
+        uniqTree[0] = 1;
+        uniqTree[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int total = 0;
+            for (int j = 1; j <= i; j++) {
+                total += uniqTree[j - 1] * uniqTree[i - j];
+            }
+
+            uniqTree[i] = total;
+        }
+
+        return uniqTree[n];
+    }
+}
