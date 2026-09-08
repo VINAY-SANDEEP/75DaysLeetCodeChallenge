3fc1f3d6f883a069a6a1a96d9b8fb62c3546b9ee
Vinay-Sandeep <133085003+VINAY-SANDEEP@users.noreply.github.com>
2026-09-08 10:06:49 +0530

    Time: 5 ms (65.87%), Space: 47.3 MB (12.32%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        int n = asteroids.length;
+        Stack<Integer> st = new Stack<Integer>();
+        for(int i=0;i<n;i++)
+        {
+            if(asteroids[i]>0)
+            {
+                st.push(asteroids[i]);
+            }else{
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(asteroids[i]))
+                {
+                    st.pop();
+                }
+                if(!st.isEmpty() && st.peek()==Math.abs(asteroids[i])){
+                         st.pop();
+                }
+else if(st.isEmpty() && asteroids[i]<0 || (st.peek()<0 && asteroids[i]<0)|| (st.peek()>0 && asteroids[i]>0))
+{
+                    st.push(asteroids[i]);
+                }
+            }
+        }
+        int y = st.size();
+        int arr[] = new int[y];
+        int i = y-1;;
+        // Reverse the stack and insert elements into the array
+        while (!st.isEmpty()) {
+            arr[i--] = st.pop();
+        }
+        return arr;
+    }
+}
